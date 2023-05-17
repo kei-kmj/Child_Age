8a43066cf07bef2469560d96b13a356c427526ca
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2023-05-17 13:31:04 +0900

    Implement child age method

--- main.rb
+++ main.rb
@@ -1,37 +1,74 @@
 # frozen_string_literal: true
 
-def child_age
-  birthday, specified_date = receive_date
+JANUARY = 1
+DECEMBER = 12
+FIRST_DAY = 1
+LAST_DAY = 31
+ONE_YEAR = 12
+YEARLY_COUNT = 10000
+MONTHLY_COUNT = 100
+
+
+#
+def child_age(birthday, specified_date)
+  return unless valid_dates?(birthday, specified_date)
 
   age = calc_age(birthday, specified_date)
   moon_age = calc_moon_age(birthday, specified_date)
 
-  if age < 1
-    "生後 #{moon_age}ヶ月"
-  elsif age < 2
-    "#{age}歳 #{moon_age}ヶ月"
+  print_child_age(age, moon_age)
+end
+
+def valid_dates?(birthday, specified_date)
+  if !valid?(birthday) || !valid?(specified_date)
+    puts '日付が不正です'
+    false
+  elsif birthday > specified_date
+    puts '指定日は誕生日より後にしてください'
+    false
   else
-    "#{age}歳"
+    true
   end
 end
 
-def receive_date
-  puts '誕生日をYYYYMMDDの形式で入力してください'
-  birthday = gets.to_i
+def valid?(date)
+  # 文字列や空文字など不正な値が入力された場合
+  return false if date.zero?
 
-  puts '指定日をYYYYMMDDの形式で入力してください'
-  specified_date = gets.to_i
-  [birthday, specified_date]
+  year = date / YEARLY_COUNT
+  month = date % YEARLY_COUNT / MONTHLY_COUNT
+  day = date % MONTHLY_COUNT
+
+  # 月又は日が不正な値の場合
+  return false if month < JANUARY || month > DECEMBER || day < FIRST_DAY || day > LAST_DAY
+
+  # 不正な年月日の場合
+  Time.new(year, month, day).strftime('%F') == \
+    "#{year.to_s.rjust(4, '0')}-#{month.to_s.rjust(2, '0')}-#{day.to_s.rjust(2, '0')}"
 end
 
 def calc_age(birthday, specified_date)
-  (specified_date - birthday) / 10_000
+  (specified_date - birthday) / YEARLY_COUNT
 end
 
 def calc_moon_age(birthday, specified_date)
-  moon_age = ((specified_date % 10_000) - (birthday % 10_000)) / 100
-  moon_age += 12 if moon_age.negative?
+  moon_age = ((specified_date % YEARLY_COUNT) - (birthday % YEARLY_COUNT)) / MONTHLY_COUNT
+  moon_age += ONE_YEAR if moon_age.negative?
   moon_age
 end
 
-puts child_age
+def print_child_age(age, moon_age)
+  if age < 1
+    puts "生後 #{moon_age}ヶ月"
+  elsif age < 2
+    puts "#{age}歳 #{moon_age}ヶ月"
+  else
+    puts "#{age}歳"
+  end
+end
+
+# エントリポイント
+# 西暦でYYYYDDMM形式で誕生日と指定日を入力する
+birthday = gets.to_i
+specified_date = gets.to_i
+child_age(birthday, specified_date)
